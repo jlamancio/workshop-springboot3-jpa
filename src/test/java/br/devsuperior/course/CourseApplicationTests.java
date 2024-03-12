db50477c794db40543c3f1f789877c9a28721edf
jlamancio <amancio.jluis@outlook.com>
2024-03-12 18:40:22 -0300

    SpringBoot_Jpa - Project created

---
+++ src/test/java/br/devsuperior/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.devsuperior.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
