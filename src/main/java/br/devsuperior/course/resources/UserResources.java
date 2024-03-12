737c51d88b08263825332f61fcb45c51db1578d6
jlamancio <amancio.jluis@outlook.com>
2024-03-12 19:13:00 -0300

    SpringBoot_Jpa - User entity and resource

---
+++ src/main/java/br/devsuperior/course/resources/UserResources.java
@@ -0,0 +1,21 @@
+package br.devsuperior.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.devsuperior.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Amancio", "amancio@email.com", "1199999999", "123456");
+		return ResponseEntity.ok().body(u);
+
+	}
+
+}
