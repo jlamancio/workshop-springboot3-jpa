7733347964fe0c7831fa73acd869d68bcba60a27
jlamancio <amancio.jluis@outlook.com>
2024-03-13 16:33:02 -0300

    SpringBoot_Jpa - Many-to-many association with JoinTable

--- src/main/java/br/devsuperior/course/config/TestConfig.java
+++ src/main/java/br/devsuperior/course/config/TestConfig.java
@@ -49,7 +49,16 @@ public class TestConfig implements CommandLineRunner {
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+				
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
 
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "João Pedro", "joao@gmail.com", "988888888", "111111");
@@ -76,8 +85,6 @@ public class TestConfig implements CommandLineRunner {
 		Order o11 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u11);
 		Order o12 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u12);
 		
-		
-
 		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6, u7, u8, u9, u10, u11, u12));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12));
 
