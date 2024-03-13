3e77c05b533dd9e5ed49898a736a1855a68994f5
jlamancio <amancio.jluis@outlook.com>
2024-03-13 10:21:48 -0300

    SpringBoot_Jpa - Order, Instant, ISO8601 - Part 1

--- src/main/java/br/devsuperior/course/entities/User.java
+++ src/main/java/br/devsuperior/course/entities/User.java
@@ -1,12 +1,15 @@
 package br.devsuperior.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -20,6 +23,10 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name, email, phone, password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	
 	public User() {}
@@ -71,6 +78,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -88,5 +99,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
