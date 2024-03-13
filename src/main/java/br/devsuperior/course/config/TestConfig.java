72cc95d0a32e8d47f6b2cf19fb1d81322025475e
jlamancio <amancio.jluis@outlook.com>
2024-03-13 11:50:34 -0300

    SpringBoot_Jpa - OrderStatus enum

--- src/main/java/br/devsuperior/course/config/TestConfig.java
+++ src/main/java/br/devsuperior/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.devsuperior.course.entities.Order;
 import br.devsuperior.course.entities.User;
+import br.devsuperior.course.entities.enums.OrderStatus;
 import br.devsuperior.course.repositories.OrderRepository;
 import br.devsuperior.course.repositories.UserRepository;
 
@@ -39,18 +40,18 @@ public class TestConfig implements CommandLineRunner {
 		User u11 = new User(null, "GAbriela Jaccomini", "gabriela@gmail.com", "999999999", "123456");
 		User u12 = new User(null, "Rainha do Abacaxi", "rainha@gmail.com", "101010101", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
-		Order o4 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u4);
-		Order o5 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u5);
-		Order o6 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u2);
-		Order o7 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u7);
-		Order o8 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u8);
-		Order o9 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u9);
-		Order o10 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u11);
-		Order o11 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u11);
-		Order o12 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u12);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED,u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED,u3);
+		Order o4 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u4);
+		Order o5 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u5);
+		Order o6 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o7 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.SHIPPED, u7);
+		Order o8 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u8);
+		Order o9 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u9);
+		Order o10 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u11);
+		Order o11 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u11);
+		Order o12 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u12);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6, u7, u8, u9, u10, u11, u12));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12));
--- src/main/java/br/devsuperior/course/entities/Order.java
+++ src/main/java/br/devsuperior/course/entities/Order.java
@@ -5,8 +5,8 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
+import br.devsuperior.course.entities.enums.OrderStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,65 +17,70 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
-	
+public class Order implements Serializable {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	public Order() {}
 
+	public Order() {
+	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public Instant getMoment() {
 		return moment;
 	}
 
-
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 
 	public User getClient() {
 		return client;
 	}
 
-
 	public void setClient(User client) {
 		this.client = client;
 	}
-
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -89,5 +94,4 @@ public class Order implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
-	
 }
