8a7ae679d1785b67b312c8330d5493a76c9f5442
jlamancio <amancio.jluis@outlook.com>
2024-03-14 07:51:22 -0300

    SpringBoot_Jpa - Payment, one-to-one association

--- src/main/java/br/devsuperior/course/config/TestConfig.java
+++ src/main/java/br/devsuperior/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.devsuperior.course.entities.Category;
 import br.devsuperior.course.entities.Order;
 import br.devsuperior.course.entities.OrderItem;
+import br.devsuperior.course.entities.Payment;
 import br.devsuperior.course.entities.Product;
 import br.devsuperior.course.entities.User;
 import br.devsuperior.course.entities.enums.OrderStatus;
@@ -99,6 +100,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 
