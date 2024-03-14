0a282e82edfebc20e74f2822e427998ae95aff56
jlamancio <amancio.jluis@outlook.com>
2024-03-14 08:10:33 -0300

    SpringBoot_Jpa - SubTotal & Total methods

--- src/main/java/br/devsuperior/course/entities/OrderItem.java
+++ src/main/java/br/devsuperior/course/entities/OrderItem.java
@@ -72,6 +72,12 @@ public class OrderItem implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 
 	@Override
 	public boolean equals(Object obj) {
