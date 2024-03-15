096db4e1614948f36cd247e8e783a20c26f47b4f
jlamancio <amancio.jluis@outlook.com>
2024-03-15 09:23:21 -0300

    Exception handling - findById

---
+++ src/main/java/br/devsuperior/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package br.devsuperior.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
