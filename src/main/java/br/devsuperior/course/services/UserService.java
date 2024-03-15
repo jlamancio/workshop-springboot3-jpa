33f41021152490a149f9aa823359e2582a947cda
jlamancio <amancio.jluis@outlook.com>
2024-03-15 08:43:04 -0300

    SpringBoot_Jpa - User update

--- src/main/java/br/devsuperior/course/services/UserService.java
+++ src/main/java/br/devsuperior/course/services/UserService.java
@@ -18,20 +18,31 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findBYId(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
 }
-
-
