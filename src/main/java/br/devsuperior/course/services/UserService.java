21632ad924c12c65d04e3ff2778a01a236de5088
jlamancio <amancio.jluis@outlook.com>
2024-03-21 07:30:34 -0300

    SpringBoot_Jpa - Exception handling update

--- src/main/java/br/devsuperior/course/services/UserService.java
+++ src/main/java/br/devsuperior/course/services/UserService.java
@@ -12,6 +12,7 @@ import br.devsuperior.course.entities.User;
 import br.devsuperior.course.repositories.UserRepository;
 import br.devsuperior.course.services.exceptions.DataBaseException;
 import br.devsuperior.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -45,9 +46,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
